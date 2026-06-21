4a5e3a87e98d9d324a0dc9d3bd6793907a08aa10
gleb-skobinsky <glebgytnik@gmail.com>
2026-06-22 00:34:09 +0300

    Remove debug mode

--- nfqws_iptables_enable.sh
+++ nfqws_iptables_enable.sh
@@ -2,7 +2,6 @@
 
 QUEUE=200
 
-# add rule with timeout safety (SSH safe)
 iptables -I FORWARD -i wg0 -j NFQUEUE --queue-num $QUEUE
 
 echo "NFQUEUE enabled."
--- start_arm64_alt9.sh
+++ start_arm64_alt9.sh
@@ -2,7 +2,6 @@ chmod +x linux-arm64/nfqws
 
 linux-arm64/nfqws \
  --qnum=200 \
- --debug=1 \
  --filter-udp=443 \
  --hostlist="lists/list-general.txt" \
  --hostlist="lists/list-general-user.txt" \
