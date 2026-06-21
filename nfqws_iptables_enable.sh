8dc51e9796d02fb8dc48d996c690c5b6144c8bb0
gleb-skobinsky <glebgytnik@gmail.com>
2026-06-22 01:51:07 +0300

    Fix iptables modification

--- nfqws_iptables_enable.sh
+++ nfqws_iptables_enable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo iptables -t mangle -A PREROUTING -p tcp --dport 80,443,2053,2083,2087,2096,8443 -j NFQUEUE --queue-num 200
+sudo iptables -t mangle -A PREROUTING -p tcp -m multiport --dports 80,443,2053,2083,2087,2096,8443 -j NFQUEUE --queue-num 200
 sudo iptables -t mangle -A PREROUTING -p udp -j NFQUEUE --queue-num 200
 
 echo "NFQUEUE enabled."
