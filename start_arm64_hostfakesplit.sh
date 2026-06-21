3593ab441fad04491585878875de1d05d48b0ddd
gleb-skobinsky <glebgytnik@gmail.com>
2026-06-22 01:09:38 +0300

    Add hostfakesplit script

---
+++ start_arm64_hostfakesplit.sh
@@ -0,0 +1,36 @@
+chmod +x linux-arm64/nfqws
+
+linux-arm64/nfqws \
+  --qnum=200 \
+  --filter-udp=443 \
+  --hostlist="lists/list-general.txt" \
+  --dpi-desync=fake \
+  --dpi-desync-repeats=6 \
+  --dpi-desync-fake-quic="lists/quic_initial_www_google_com.bin" \
+  --new \
+  --filter-udp=19294-19344,50000-50100 \
+  --filter-l7=discord,stun \
+  --dpi-desync=fake \
+  --dpi-desync-repeats=6 \
+  --new \
+  --filter-tcp=2053,2083,2087,2096,8443 \
+  --hostlist-domains=discord.media \
+  --dpi-desync=hostfakesplit \
+  --dpi-desync-repeats=4 \
+  --dpi-desync-fooling=ts \
+  --dpi-desync-hostfakesplit-mod=host=www.google.com \
+  -- new \
+  --filter-tcp=443 \
+  --hostlist="lists/list-google.txt" \
+  --ip-id=zero \
+  --dpi-desync=hostfakesplit \
+  --dpi-desync-repeats=4 \
+  --dpi-desync-fooling=ts \
+  --dpi-desync-hostfakesplit-mod=host=www.google.com \
+  --new \
+  --filter-tcp=80,443 \
+  --hostlist="lists/list-general.txt" \
+  --dpi-desync=hostfakesplit \
+  --dpi-desync-repeats=4 \
+  --dpi-desync-fooling=ts \
+  --dpi-desync-hostfakesplit-mod=host=www.google.com
