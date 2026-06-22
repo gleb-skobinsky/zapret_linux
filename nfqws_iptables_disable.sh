a460d5d7016901a95f1ce985ebdf88db2d3ca79e
gleb-skobinsky <glebgytnik@gmail.com>
2026-06-22 12:06:42 +0300

    Temporarily disable prerouting rules

--- nfqws_iptables_disable.sh
+++ nfqws_iptables_disable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo iptables -t mangle -D OUTPUT -p tcp -m multiport --dports 80,443,2053,2083,2087,2096,8443 -j NFQUEUE --queue-num 200
-sudo iptables -t mangle -D OUTPUT -p udp -j NFQUEUE --queue-num 200
-sudo iptables -t mangle -D PREROUTING -p tcp -m multiport --dports 80,443,2053,2083,2087,2096,8443 -j NFQUEUE --queue-num 200
-sudo iptables -t mangle -D PREROUTING -p udp -j NFQUEUE --queue-num 200
+sudo iptables -t mangle -D POSTROUTING -p tcp -m multiport --dports 80,443,2053,2083,2087,2096,8443 -j NFQUEUE --queue-num 200
+sudo iptables -t mangle -D POSTROUTING -p udp -j NFQUEUE --queue-num 200
+# sudo iptables -t mangle -D PREROUTING -p tcp -m multiport --dports 80,443,2053,2083,2087,2096,8443 -j NFQUEUE --queue-num 200
+# sudo iptables -t mangle -D PREROUTING -p udp -j NFQUEUE --queue-num 200
 
 echo "NFQUEUE disabled."
